3bc121d41fb4ac2b9d98739fc20044d647c0f26a
XaaRii <pgambler@seznam.cz>
2023-04-07 21:35:45 +0200

    cocogun

--- commands/pull.js
+++ commands/pull.js
@@ -6,7 +6,7 @@ module.exports = {
 	usage: `<where? path>`,
 	showHelp: false,
 	async execute(message, args) {
-		if (!config.admins.includes(message.author.id)) return message.reply("*You tried to pull some bitches, but...*\n  Don't you dare hunt in **my** territory! <:cocoGun:1037328931386298378>");
+		if (!config.admins.includes(message.author.id)) return message.reply("*You tried to pull some bitches, but...*\n  Don't you dare hunt in **my** territory! <a:cocoGun:1037328931386298378>");
 		if (!args[0]) return message.channel.send("Please specify a path of the file you want! (name included ofc)\nSomething like: `/home/pi/test.png` or `C:/Users/username/Desktop/file.png` or `./heya.png`");
 		message.channel.send({ content: "Here you go.", files: [args[0]] }).catch(err => {
 			message.channel.send('I can\'t find that file! sry');
